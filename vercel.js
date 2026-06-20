4ed75ef39c5f3bbdbb77b4357e062436320e7761
Mojtaba <mojtaba1231384@gmail.com>
2026-06-20 18:34:01 +0330

    fix: remove functions config from vercel.js

--- vercel.js
+++ vercel.js
@@ -1,12 +1,5 @@
-/** @type {import('@vercel/node').VercelConfig} */
-export default {
+export const config = {
   framework: 'nextjs',
   buildCommand: 'npm run build',
-  outputDirectory: '.next',
-  regions: ['iad1'],
-  functions: {
-    'api/**/*.js': {
-      maxDuration: 10
-    }
-  }
+  outputDirectory: '.next'
 };
